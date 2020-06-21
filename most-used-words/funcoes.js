f30f7f255a32e715480dace0f39d5c650af0c5fa
Bruno Bach <brunobach_sl@hotmail.com>
2020-06-21 00:55:32 -0300

    Adicionando Funcao agrupar palavras

--- most-used-words/funcoes.js
+++ most-used-words/funcoes.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const path = require('path')
-const { promises } = require('dns')
 
 function lerDiretorio(caminho) {
     return new Promise((resolve, reject) => {
@@ -51,6 +50,38 @@ function removerSeApenasNumeros(array) {
     })
 }
 
+function removerCaracteres(simbolos){
+    return function(array){
+        return array.map(el => {
+            return simbolos.reduce((acc, el) => {
+                console.log(el)
+               return acc.split(el).join('')
+            }, el)
+        })
+    }
+}
+function agruparPalavras(palavras){
+    return Object.values(palavras.reduce((acc, palavra) => {
+        const el = palavra.toLowerCase()
+        const qtde = acc[el] ? acc[el].qtde + 1 : 1 
+        acc[el] = {
+            elemento: el,
+            qtde
+        }
+        return acc
+        
+    }, {}))
+}
+
+function ordenarPorAtribNumerico(attr, ordem = 'asc'){
+    return function(array){
+        const desc = (o1, o2) => o2[attr] - o1[attr]
+        const asc = (o1, o2) => o1[attr] - o2[attr]
+        
+        return array.sort(ordem === 'asc' ? asc : desc)
+    }
+}
+
 module.exports = {
     lerDiretorio,
     elementosTerminadosCom,
@@ -58,5 +89,8 @@ module.exports = {
     lerArquivos,
     removerSeVazio,
     removerseIncluir,
-    removerSeApenasNumeros
+    removerSeApenasNumeros,
+    removerCaracteres,
+    agruparPalavras,
+    ordenarPorAtribNumerico
 }
--- most-used-words/index.js
+++ most-used-words/index.js
@@ -1,16 +1,34 @@
 const path = require('path')
 const fn = require('./funcoes')
-const { elementosTerminadosCom, lerArquivos, removerSeVazio, removerseIncluir, removerSeApenasNumeros } = require('./funcoes')
+const { elementosTerminadosCom, lerArquivos, removerSeVazio, removerseIncluir, removerSeApenasNumeros, removerCaracteres, agruparPalavras, ordenarPorAtribNumerico } = require('./funcoes')
 
 const caminho = path.join(__dirname, '..', 'legendas')
+
+const simbolos = [
+    '.', '?', '-', ',', '"', '♪',
+    '_', '<i>', '</i>', '\r', '[', ']',
+    '(', ')', '!'
+]
+
+const mesclarConteudos = array => array.join(' ')
+const separarPorLinhas = todoConteudo => todoConteudo.split('\n')
+const separarPorPalavras = todoConteudo => todoConteudo.split(' ')
+
+
 
 fn.lerDiretorio(caminho)
     .then(arquivos => elementosTerminadosCom(arquivos, '.srt'))
     .then(caminhos => lerArquivos(caminhos))
-    .then(conteudos => conteudos.join('\n'))
-    .then(todoConteudo => todoConteudo.split('\n'))
+    .then(mesclarConteudos)
+    .then(separarPorLinhas)
     .then(removerSeVazio)
     .then(removerseIncluir('-->'))
     .then(removerSeApenasNumeros)
-    .then(console.log)
-
+    .then(removerCaracteres(simbolos))
+    .then(mesclarConteudos)
+    .then(separarPorPalavras)
+    .then(removerSeVazio)
+    .then(removerSeApenasNumeros)
+    .then(fn.agruparPalavras)
+    .then(ordenarPorAtribNumerico('qtde', 'desc'))
+    //.then(console.log)
